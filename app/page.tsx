14694157d4a2ab3a3b831f3de015b2ba8905287b
jacklion710 <jack.lion710@gmail.com>
2024-01-13 23:05:23 -0500

    More vectors and colors, & filter IDs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 // pages/example.js
 import React from 'react';
 import { applySvgFilter } from '../components/SVGFilter';
+import { HStack } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <div>
-      {applySvgFilter('/next.svg', 'myFilter', '#ff0000', { width: 300, height: 200 })}
-      {applySvgFilter('/vercel.svg', 'myFilter', '#00ff00', { width: 300, height: 200 })}
+      <HStack>
+        {applySvgFilter('/vercel.svg', 'vercelFilter1', '#ff0000', { width: 300, height: 200 })}
+        {applySvgFilter('/vercel.svg', 'vercelFilter2', '#00ff00', { width: 300, height: 200 })}
+        {applySvgFilter('/vercel.svg', 'vercelFilter3', '#0000ff', { width: 300, height: 200 })}
+      </HStack>
+      <HStack>
+        {applySvgFilter('/next.svg', 'nextFilter1', '#ff0000', { width: 300, height: 200 })}
+        {applySvgFilter('/next.svg', 'nextFilter2', '#00ff00', { width: 300, height: 200 })}
+        {applySvgFilter('/next.svg', 'nextFilter3', '#0000ff', { width: 300, height: 200 })}
+      </HStack>
     </div>
   );
 };
