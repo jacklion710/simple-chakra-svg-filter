ba1691b13c492690504f7ceca128b03dea10bff4
jacklion710 <jack.lion710@gmail.com>
2024-01-13 23:01:28 -0500

    Reusable SVG color filter component

---
+++ components/SVGFilter.tsx
@@ -0,0 +1,35 @@
+// components/SVGFilter.tsx
+import React from 'react';
+import { Image } from '@chakra-ui/react';
+
+interface SVGFilterProps {
+  src: string;
+  filterId: string;
+  color: string;  // Hex color for the filter
+  [key: string]: any; // This is for otherProps, allowing any additional props
+}
+
+const SVGFilter: React.FC<SVGFilterProps> = ({ src, filterId, color, ...rest }) => {
+  return (
+    <>
+      <svg width="0" height="0">
+        <filter id={filterId}>
+          <feFlood floodColor={color} result="flood" />
+          <feComposite in="flood" in2="SourceGraphic" operator="in" />
+        </filter>
+      </svg>
+      <Image src={src} style={{ filter: `url(#${filterId})` }} {...rest} />
+    </>
+  );
+};
+
+export const applySvgFilter = (
+  src: string, 
+  filterId: string, 
+  color: string, 
+  otherProps: { [key: string]: any }
+) => {
+  return <SVGFilter src={src} filterId={filterId} color={color} {...otherProps} />;
+};
+
+export default SVGFilter;
