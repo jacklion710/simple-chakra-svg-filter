544c917b8f17dcd7249fb9a83f65e1527ebd5fa4
jacklion710 <jack.lion710@gmail.com>
2024-01-16 19:13:39 -0500

    Wrapped page in chakraprovider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-// pages/example.js
 import React from 'react';
 import { applySvgFilter } from '../components/SVGFilter';
-import { HStack } from '@chakra-ui/react';
+import { HStack, ChakraProvider } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <div>
+    <ChakraProvider>
       <HStack>
         {applySvgFilter('/vercel.svg', 'vercelFilter1', '#ff0000', { width: 300, height: 200 })}
         {applySvgFilter('/vercel.svg', 'vercelFilter2', '#00ff00', { width: 300, height: 200 })}
@@ -16,7 +15,7 @@ const Home = () => {
         {applySvgFilter('/next.svg', 'nextFilter2', '#00ff00', { width: 300, height: 200 })}
         {applySvgFilter('/next.svg', 'nextFilter3', '#0000ff', { width: 300, height: 200 })}
       </HStack>
-    </div>
+    </ChakraProvider>
   );
 };
 
